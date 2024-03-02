2970872a4b4acefc4c86d10717bbc936e0900c71
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-02 19:22:33 +0100

    fix: routes fixed, added home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { RouterOutlet, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { ClimaComponent } from './pages/clima/clima.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
 
-{path: '', component:RouterOutlet},
+{path: '', component:HomeComponent},
 {path: 'clima', component:ClimaComponent},
 
 {path: '**',redirectTo: '',pathMatch:'full' },
