a08c225f3baafd990f1764ee90212fcea5f819aa
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-02 19:04:25 +0100

    feat: app.component.html changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports:
+    [
+
+      HeaderComponent,
+      SidebarComponent,
+      RouterOutlet,
+      FooterComponent,
+
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cliente';
+  title = 'grid-proyect';
 }
