df897ea33a9cb48ffa25ad19d4112c0f41c37927
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-02 20:59:26 +0100

    feat: cats.service completed

--- src/app/services/cats.service.ts
+++ src/app/services/cats.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class CatsService {
-
+  private _http = inject(HttpClient);
+  private urlBase: string = 'https://cataas.com/cat?type=square&fit=cover&position=centre&width=500&height=500';
+  getCats(): Observable<any> {
+    return this._http.get(this.urlBase);
+  }
   constructor() { }
 }
