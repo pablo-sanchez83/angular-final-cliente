6cb8f1149ac759338e81c63009cd0daede00a0ca
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-02 19:14:29 +0100

    fix: routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { RouterOutlet, Routes } from '@angular/router';
+import { ClimaComponent } from './pages/clima/clima.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{path: '', component:RouterOutlet},
+{path: 'clima', component:ClimaComponent},
+
+{path: '**',redirectTo: '',pathMatch:'full' },
+
+];
