b688695929f55a0fcdc48e810f6a9a87281641cf
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-02 19:28:15 +0100

    feat: clima.service.ts completed

--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+ providedIn: 'root'
 })
 export class ClimaService {
+ private _http = inject(HttpClient);
+ private urlBase = 'https://api.openweathermap.org/data/2.5/weather';
+ private apiKey = '605507acf87117e111e54a3ab5238541';
+ private difKelvin = 273.15;
 
-  constructor() { }
-}
+ buscarClima(ciudad: string): Observable<any> {
+  return this._http.get(`${this.urlBase}?q=${ciudad}&appid=${this.apiKey}`);
+ }
+
+ procesarDatosClima(data: any): any {
+  return {
+   ciudadNombre: data.name,
+   paisNombre: data.sys.country,
+   temperatura: Math.floor(data.main.temp - this.difKelvin),
+   humedad: data.main.humidity,
+   descripcion: data.weather[0].description,
+   icono: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+  };
+ }
+} 
