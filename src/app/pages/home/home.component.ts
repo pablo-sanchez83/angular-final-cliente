0653088bf00ad6774fd0f5d64a3233260029b0a4
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-02 20:01:21 +0100

    feat: upgraded home.component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
  selector: 'app-home',
  templateUrl: './home.component.html',
  styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
- visitCount: number = 0;
+export class HomeComponent {
  appName: string = 'MiApp';
 
  constructor() { }
-
- ngOnInit(): void {
-  // No incrementamos el contador al inicializar el componente
- }
-
- incrementVisitCount() {
-  // Incrementamos el contador de visitas al hacer clic en el botón
-  this.visitCount += 1;
- }
 }
