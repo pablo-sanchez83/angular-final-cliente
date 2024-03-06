885785fba94fe774c0976d0140938f30576b92a9
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-06 14:31:59 +0100

    fix:fixed data show private service component; refactor: usuario.component deleted for not usage

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,22 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioService } from '../../services/api.service';
-import { UsuarioComponent } from '../../components/usuario/usuario.component';
 
 @Component({
   selector: 'app-usuario-lista',
   standalone: true,
-  imports: [UsuarioComponent, CommonModule],
+  imports: [CommonModule],
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
   usuarios: any[] = [];
-
   constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
-    this.usuarioService.getUsuarios(0, 10).subscribe(data => {
+    this.usuarioService.getUsuarios(0, 100).subscribe(data => {
       this.usuarios = data.usuarios;
     });
   }
