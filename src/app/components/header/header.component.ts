237dbcdd1c16b881f3903dd26608b9d3942a74a6
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-07 15:04:29 +0100

    feat:header.component completed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
-
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -12,5 +11,24 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  search: string = ''
+  search: string = '';
+  constructor(private router: Router) { }
+  buscar() {
+    switch (this.search.toLowerCase()) {
+      case 'cats':
+        this.router.navigate(['/cats']);
+        break;
+      case 'clima':
+        this.router.navigate(['/clima']);
+        break;
+      case 'gitHub':
+        this.router.navigate(['/github']);
+        break;
+      case 'servidor':
+        this.router.navigate(['/server']);
+        break;
+      default:
+        this.router.navigate(['/home']);
+    }
+  }
 }
