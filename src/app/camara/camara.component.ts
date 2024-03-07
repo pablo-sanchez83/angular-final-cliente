f119e891cbf804a9cabefacfce6476efeacd7e68
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-07 15:06:02 +0100

    feat: base calculadora;camara;galeria component

---
+++ src/app/camara/camara.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-camara',
+  standalone: true,
+  imports: [],
+  templateUrl: './camara.component.html',
+  styleUrl: './camara.component.css'
+})
+export class CamaraComponent {
+
+}
