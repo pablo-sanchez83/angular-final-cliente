0b330acbb47df13f11d59b7b192a16ef03f6d88e
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-08 03:33:35 +0100

    me rindo con el tictactoe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { CatsComponent } from './pages/cats/cats.component';
 import { GithubComponent } from './pages/github/github.component';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 import { CalculadoraComponent } from './pages/calculadora/calculadora.component';
-import { TictactoeComponent } from './pages/tictactoe/tictactoe.component';
+import { TicTacToeComponent } from './pages/tictactoe/tictactoe.component';
 export const routes: Routes = [
 
 {path: 'home', component:HomeComponent},
@@ -14,6 +14,6 @@ export const routes: Routes = [
 {path: 'github', component:GithubComponent},
 {path: 'server', component:UsuariosComponent},
 {path: 'calculadora', component:CalculadoraComponent},
-{path: 'tictactoe', component:TictactoeComponent},
+{path: 'tictactoe', component:TicTacToeComponent},
 {path: '**',redirectTo: 'home',pathMatch:'full' },
 ];
