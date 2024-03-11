e7eca4787f4d969bc097068e2ec53f696b4524ab
pablosanchez <pablosanchezperianez83@gmail.com>
2024-03-11 21:58:32 +0100

    refactor:code commented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,18 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports:
+@Component({  // Esto es un decorador que marca una clase como un componente de Angular y proporciona metadatos de configuración.
+  selector: 'app-root',  // Este es el selector del componente. Se utiliza para identificar este componente en las plantillas HTML.
+  standalone: true,  // Esto significa que el componente puede existir independientemente, sin ser parte de un módulo.
+  imports:  // Esto define los componentes o módulos que se importarán en este componente.
     [
-
-      HeaderComponent,
-      RouterOutlet,
-      FooterComponent,
-
+      HeaderComponent,  // Este es probablemente un componente de encabezado.
+      RouterOutlet,  // Esto es para la funcionalidad de enrutamiento en Angular.
+      FooterComponent,  // Este es probablemente un componente de pie de página.
     ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html',  // Esto es la ubicación del archivo de plantilla HTML para este componente.
+  styleUrl: './app.component.css'  // Esto es la ubicación del archivo de estilos CSS para este componente.
 })
-export class AppComponent {
-  title = 'grid-proyect';
+export class AppComponent {  // Esto es la definición de la clase del componente.
+  title = 'grid-proyect';  // Esto es una propiedad de la clase que probablemente se utiliza en la plantilla HTML.
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,7 +5,8 @@ import { Component} from '@angular/core';
  templateUrl: './home.component.html',
  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
- img = 'https://picsum.photos/400/200';
- constructor() { }
-}
+export class HomeComponent {  // Esto es la definición de la clase del componente.
+    img = 'https://picsum.photos/400/200';  // Esto es una propiedad de la clase que probablemente se utiliza para mostrar una imagen en la plantilla HTML.
+    constructor() { }  // Esto es el constructor de la clase. Se invoca cuando se crea una instancia de la clase.
+   }
+   
